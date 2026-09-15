475c29ca8d731e0e7ae02a3bcfce26f47772c179
HiHelloItsMe <110574186+hihelloitsme1@users.noreply.github.com>
2026-09-15 12:38:57 -0700

    Anim,platform,map
    Player movement animation, rising platform fix, map changes

--- doe-platformer/Scripts/player.gd
+++ doe-platformer/Scripts/player.gd
@@ -2,27 +2,36 @@ extends CharacterBody2D
 
 @export var animated_sprite_2d: AnimatedSprite2D
 #
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
 
 func _ready():
 	animated_sprite_2d.play("idle")
 
-#func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction:
+		animated_sprite_2d.flip_h = direction < 0
+	
+	if is_on_floor():
+		if direction:
+			animated_sprite_2d.play("run")
+		else:
+			animated_sprite_2d.play("idle")
+	else:
+		if velocity.y < 100:
+			animated_sprite_2d.play("jump")
+		else:
+			animated_sprite_2d.play("fall")
+	move_and_slide()
---
+++ doe-platformer/Scripts/rising_platform.gd
@@ -0,0 +1,30 @@
+extends StaticBody2D
+
+@onready var timer = $Timer
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+@export var bounds: Vector2;
+@export var speed: float = 1;
+@export var pause_time: float = 1;
+@export var start_direction: int = 1;
+var direction: int;
+var go: bool = true;
+
+func _ready():
+	direction = start_direction;
+	timer.wait_time = pause_time;
+	animated_sprite_2d.play("move");
+
+func _physics_process(delta):
+	if position.y < bounds.x or position.y > bounds.y:
+		direction *= -1;
+		timer.start()
+		go = false;
+		position.y = bounds.x if position.y < bounds.x else bounds.y;
+		animated_sprite_2d.stop();
+	if (go):
+		position.y += speed * direction;
+
+func _on_timer_timeout():
+	go = true
+	animated_sprite_2d.play("move");
