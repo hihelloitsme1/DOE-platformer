e54eef0cd28b9ce88af72cff802a80c3bacad6c9
HiHelloItsMe <110574186+hihelloitsme1@users.noreply.github.com>
2026-09-18 11:43:05 -0700

    death
    changes to death to make it better

--- doe-platformer/Scripts/player.gd
+++ doe-platformer/Scripts/player.gd
@@ -5,13 +5,19 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
+var dead;
+
 func _ready():
+	dead = false;
 	animated_sprite_2d.play("idle")
+	Engine.time_scale = 1;
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	if dead: return;
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -37,4 +43,10 @@ func _physics_process(delta):
 	move_and_slide()
 
 func die():
-	get_tree().reload_current_scene();
+	dead = true;
+	animated_sprite_2d.play("hit");
+	Engine.time_scale = 0.5;
+
+func _on_animated_sprite_2d_animation_looped():
+	if (animated_sprite_2d.animation == "hit"):
+		get_tree().reload_current_scene();
